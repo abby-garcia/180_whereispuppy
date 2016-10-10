70873abdc484721ab69bb9c08b870c3102bd6665
abby-garcia <acg361@gmail.com>
2016-10-10 17:21:25 -0400

    one box is working

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $( document ).ready(function() {
     
-$( ".box1" ).on( "click", function() {
-	// $(this).hide();
-  	$(".puppy_pic").show();
+$( ".box1" ).on( "click", function(e) {
+  	$(e.target).hide();
+  	$('.puppy_pic').show();
 });
 
-$( ".box2" ).on( "click", function() {
-	$('.box2').hide();
-  	$(".puppy_pic").show();
-});
+// $( ".box2" ).on( "click", function() {
+// 	$('.box2').hide();
+//   	$(".puppy_pic").show();
+// });
 
 
 

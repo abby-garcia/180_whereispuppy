901a6330dc25c3bfaf3a6c339a512c1f683c2986
abby-garcia <acg361@gmail.com>
2016-10-10 16:58:08 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+    
+$( ".box1" ).on( "click", function() {
+	// $(this).hide();
+  	$(".puppy_pic").show();
+});
+
+$( ".box2" ).on( "click", function() {
+	$('.box2').hide();
+  	$(".puppy_pic").show();
+});
+
+
+
+
+
+
+
+
+
+
+
+});  //jquery ends
